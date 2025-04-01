59d8b500bd82bc72d622dd4dc3f6bb1f4bbcd1ba
Aleksey Bershov <bershov.aleksey@bk.ru>
2025-04-01 13:31:34 +0300

    Initial commit

---
+++ src/main/java/ru/kata/springbootInit/SpringbootInitApplication.java
@@ -0,0 +1,13 @@
+package ru.kata.springbootInit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInitApplication.class, args);
+	}
+
+}
